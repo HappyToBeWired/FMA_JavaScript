50467120c6271070c2c71d5ad1b490c748b24638
Alex <happytobewired@gmail.com>
2012-07-13 01:29:40 +0100

    small change in json

--- WebContent/json(temp).js
+++ WebContent/json(temp).js
@@ -1,16 +1,17 @@
 
   var drinksAndSmoothies =   { "stock_status":     [
-  {"product_id":"001","product_type":"smoothie","product_name":"Rasberry & Pear","brief_description":"","in_stock_quantity":"","ordered_quantity":"","image_location":""},
-  {"product_id":"002","product_type":"smoothie","product_name":"Apricote & Dandilion","brief_description":"","in_stock_quantity":"","ordered_quantity":"","image_location":""},
-  {"product_id":"003","product_type":"juice","product_name":"Ginger & Orange","brief_description":"","in_stock_quantity":"","ordered_quantity":"","image_location":""},
-  {"product_id":"004","product_type":"smoothie","product_name":"Prune & Brussel Sprout","brief_description":"","in_stock_quantity":"","ordered_quantity":"","image_location":""},
-  {"product_id":"005","product_type":"juice","product_name":"Strawberry & Lemongrass","brief_description":"","in_stock_quantity":"","ordered_quantity":"","image_location":""},
-  {"product_id":"006","product_type":"smoothie","product_name":"Plum & Juniper","brief_description":"","in_stock_quantity":"","ordered_quantity":"","image_location":""},
-  {"product_id":"007","product_type":"juice","product_name":"Grape & Honeysuckle","brief_description":"","in_stock_quantity":"","ordered_quantity":"","image_location":""},
-  {"product_id":"008","product_type":"juice","product_name":"Watercress & Orange","brief_description":"","in_stock_quantity":"","ordered_quantity":"","image_location":""},
-  {"product_id":"009","product_type":"smoothie","product_name":"Plum & Pommegranate","brief_description":"","in_stock_quantity":"","ordered_quantity":"","image_location":""},
-  {"product_id":"010","product_type":"juice","product_name":"Cow & Gate","brief_description":"","in_stock_quantity":"","ordered_quantity":"","image_location":""},
-  {"product_id":"011","product_type":"smoothie","product_name":"Cheech & Chong","brief_description":"","in_stock_quantity":"","ordered_quantity":"","image_location":""}
+  {"product_id":"001","product_type":"smoothie","product_name":"Rasberry & Pear","brief_description":"","in_stock_quantity":"","ordered_quantity":"","image_location":"","price":""},
+  {"product_id":"002","product_type":"smoothie","product_name":"Apricote & Dandilion","brief_description":"","in_stock_quantity":"","ordered_quantity":"","image_location":"","price":""},
+  {"product_id":"003","product_type":"juice","product_name":"Ginger & Orange","brief_description":"","in_stock_quantity":"","ordered_quantity":"","image_location":"","price":""},
+  {"product_id":"004","product_type":"smoothie","product_name":"Prune & Brussel Sprout","brief_description":"","in_stock_quantity":"","ordered_quantity":"","image_location":"","price":""},
+  {"product_id":"005","product_type":"juice","product_name":"Strawberry & Lemongrass","brief_description":"","in_stock_quantity":"","ordered_quantity":"","image_location":"","price":""},
+  {"product_id":"006","product_type":"smoothie","product_name":"Plum & Juniper","brief_description":"","in_stock_quantity":"","ordered_quantity":"","image_location":"","price":""},
+  {"product_id":"007","product_type":"juice","product_name":"Grape & Honeysuckle","brief_description":"","in_stock_quantity":"","ordered_quantity":"","image_location":"","price":""},
+  {"product_id":"008","product_type":"juice","product_name":"Watercress & Orange","brief_description":"","in_stock_quantity":"","ordered_quantity":"","image_location":"","price":""},
+  {"product_id":"009","product_type":"smoothie","product_name":"Plum & Pommegranate","brief_description":"","in_stock_quantity":"","ordered_quantity":"","image_location":"","price":""},
+  {"product_id":"010","product_type":"juice","product_name":"Cow & Gate","brief_description":"","in_stock_quantity":"","ordered_quantity":"","image_location":"","price":""},
+  {"product_id":"011","product_type":"smoothie","product_name":"Cheech & Chong","brief_description":"","in_stock_quantity":"","ordered_quantity":"","image_location":"","price":""}
   
   ]
+ 
      };
