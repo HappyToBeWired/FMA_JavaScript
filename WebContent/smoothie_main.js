12f97f5dd11f48ece3b344cb3319e583ee8576de
Alex <happytobewired@gmail.com>
2012-07-18 01:09:23 +0100

    a bit more fiddling with layout

--- WebContent/smoothie_main.js
+++ WebContent/smoothie_main.js
@@ -1,8 +1,9 @@
 function smoothieOrder() {
   
 
-var drinksAndSmoothies =   { "stock_status":     [
-  {"product_id":"001","product_type":"smoothie","product_name":"Rasberry & Pear","brief_description":"Healthy combination of British fruits with a banana base.","in_stock_quantity":"50","ordered_quantity":"","image_location":"","price":"10.00"},
+var drinksAndSmoothies =   
+{ "stock_status":     [
+  {"product_id":"001","product_type":"smoothie","product_name":"Raspberry & Pear","brief_description":"Healthy combination of British fruits with a banana base.","in_stock_quantity":"50","ordered_quantity":"","image":"pearAndraspbery.jpeg","price":"10.00"},
   {"product_id":"002","product_type":"smoothie","product_name":"Apricot & Dandilion","brief_description":"Weeds and a furry plum yum.","in_stock_quantity":"40","ordered_quantity":"","image_location":"","price":"12.00"},
   {"product_id":"003","product_type":"juice","product_name":"Ginger & Orange","brief_description":" A refreshing flavoured juice. ","in_stock_quantity":"80","ordered_quantity":"","image_location":"","price":"9.00"},
   {"product_id":"004","product_type":"smoothie","product_name":"Prune & Brussel Sprout","brief_description":"An explosive combination.","in_stock_quantity":"100","ordered_quantity":"","image_location":"","price":"11.00"},
@@ -14,33 +15,42 @@ var drinksAndSmoothies =   { "stock_status":     [
   {"product_id":"010","product_type":"juice","product_name":"Cow & Gate","brief_description":"More drink.","in_stock_quantity":"0","ordered_quantity":"58","image_location":"","price":"12.00"},
   {"product_id":"011","product_type":"smoothie","product_name":"Cheech & Chong","brief_description":"Another pointless combination.","in_stock_quantity":"12","ordered_quantity":"","image_location":"","price":"13.00"}
   
-  ],
+                     ],
   "shopping_cart": [
      {"product_id":"001","ordered_quantity":"" }           
                     
      ]
-     };
-  
-  
-  
+};
 
-// truncates 2 parts of json stock status  name
- var stockView = drinksAndSmoothies.stock_status;
+
+
+  
+//truncates 2 parts of json stock status  name
+var stockView = drinksAndSmoothies.stock_status;  
+
+
+
+
+
  var results = "";
 // outputs stock in divs
-  for(var i=0; i<stockView.length; i++) {
+  for(var i=0; i<stockView.length; i++) 
+  {
 	  
 	  results += "<div class='display'>";
-      results += "<p>" + stockView[i].product_id + "</p>";  
-      results += "<p>" + stockView[i].product_type + "</p>"; 
-      results += "<h3>" + stockView[i].product_name + "</h3>";
-      results += "<p>" + stockView[i].in_stock_quantity + "</p>";
+	  results += "<h3>" + stockView[i].product_name + "</h3>";
+	  results += "<p>Product Number: <span>"; 
+      results +=  stockView[i].product_id + "</span></p>";
+      results += "<p>Product Type: <span>";
+      results += stockView[i].product_type + "</span></p>"; 
+      results += "<p>£<span>";
+      results += stockView[i].price + "</span> per case.</p>"; 
+      results += "<p>In Stock: <span>";
+      results += stockView[i].in_stock_quantity + "</span></p>";
+      results += "<img src='images/" + stockView[i].image + "' alt='" +stockView[i].image+ "' height='100' width='100' />";
       results += "</div>";
 
   }
-
- // results += "<table><br /> <br />";
-
   var div = document.getElementById("output");
 
       div.innerHTML = results;    
